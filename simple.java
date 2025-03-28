3c48c5b729b3f1e5082ce1de5403873ec6885d40
veeres-javali18 <veeresjavali038@gmail.com>
2025-03-28 10:51:31 +0530

    update this file

--- simple.java
+++ simple.java
@@ -1,6 +1,7 @@
 public class simple{
       public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("This is java file");
         
      }
 }
